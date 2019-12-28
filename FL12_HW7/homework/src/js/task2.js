a570931189b9f00dfed15c4db433b40986df4e6d
Sergey Migulin <sergiomigulini@gmail.com>
2019-12-28 17:47:29 +0200

    finished task-2

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,42 +1,68 @@
 let willPlay = confirm('Do you want to play a game?');
 const prize = {
     total: 0,
-    possible: 100
+    continuousFactor: 1,
+    firstPossible: 100,
+    secondPossible: 50,
+    thirdPossible: 25,
+    currentPossible: null
 }
 const range = {
     start: 0,
-    end: 8
+    end: 8,
+    increment: 4
 }
 const user = {
     choise: null,
     attempt: 0,
     attemptions: 3
 }
+let bufferConversionChecker, diceNumber;
 
 if(!willPlay){
     alert('You did not become a billionaire, but can.');
 }  
-    // const diceNumber = Math.floor(Math.random() * range.end + 1);
     
-for(let bufferConversionChecker, diceNumber, casinoAttemptsCounter = 1;
-    willPlay; // && casinoAttemptsCounter <= user.attemptions;
-    diceNumber = Math.floor(Math.random() * range.end + 1),
-    casinoAttemptsCounter++
-){
-    user.choise = (bufferConversionChecker = prompt(`
-        Choose a roulette pocket number from ${range.start} to ${range.end}
-        Attempts left: ${user.attemptions}
-        Total prize: ${prize.total}$
-        Possible prize on current attempt: ${prize.possible}$
-    `)) === '' || null
-    ? NaN
-    : bufferConversionChecker;
+for(bufferConversionChecker, 
+    diceNumber = Math.floor(Math.random() * Math.floor(range.end + 1)),
+    prize.currentPossible = prize.firstPossible * prize.continuousFactor;
 
-    //game with 3 attempts
-    user.attempt++;
+    willPlay;
+    
+    diceNumber = Math.floor(Math.random() * Math.floor(range.end + 1))
+){
+
+    if(user.attempt <= user.attemptions){
+        user.choise = (bufferConversionChecker = prompt(`
+        Choose a roulette pocket number from ${range.start} to ${range.end}
+        Attempts left: ${user.attemptions - user.attempt}
+        Total prize: ${prize.total}$
+        Possible prize on current attempt: ${prize.currentPossible}$
+            `)) === ('' || null)
+            ? NaN
+            : Number(bufferConversionChecker);
+        
+        user.attempt++;
+
+    }
+
+    
+
     if(user.choise === diceNumber && 
         user.attempt <= user.attemptions){
-        prize.total += prize.possible;
+
+        if(user.attempt === 1){
+            prize.currentPossible = prize.firstPossible * prize.continuousFactor;
+            prize.total += prize.currentPossible;
+        }
+        if(user.attempt === 2){
+            prize.currentPossible = prize.secondPossible * prize.continuousFactor;
+            prize.total += prize.currentPossible;
+        }
+        if(user.attempt === 3){
+            prize.currentPossible = prize.thirdPossible * prize.continuousFactor;
+            prize.total += prize.currentPossible;
+        }
 
         willPlay = confirm(`
         Congratulation, you won!   
@@ -45,12 +71,55 @@ for(let bufferConversionChecker, diceNumber, casinoAttemptsCounter = 1;
         `);
 
         if(willPlay){
-            continue;
+
+            user.attempt = 0;
+            range.end += range.increment;
+
+            prize.continuousFactor++;
+            prize.firstPossible *= prize.continuousFactor;
+            prize.secondPossible = Math.floor(prize.firstPossible / 2);
+            prize.thirdPossible = Math.floor(prize.secondPossible / 2);
+            prize.currentPossible = prize.firstPossible * prize.continuousFactor;
         }else{
-            willPlay = false;
+
+            user.attempt = 0;
+            range.end = 8;
+            alert(`
+            Thank you for your participation. 
+            Your prize is: ${prize.total}$
+            `);
+
+            willPlay = confirm(`
+                Do you want to play again?
+            `);
+            if(willPlay){
+
+                range.end = 8;
+
+                prize.continuousFactor = 1;
+                prize.total = 0;
+                prize.firstPossible = 100;
+                prize.secondPossible = Math.floor(prize.firstPossible / 2);
+                prize.thirdPossible = Math.floor(prize.secondPossible /2);
+                prize.currentPossible = prize.firstPossible * prize.continuousFactor;
+            }
         }
     }else{
-        // after fail attempt
+        if(user.attemptions - user.attempt === 3){
+            prize.currentPossible = prize.firstPossible * prize.continuousFactor;
+            continue;
+        }
+        if(user.attemptions - user.attempt === 2){
+            prize.currentPossible = prize.secondPossible * prize.continuousFactor;
+            continue;
+        }
+        if(user.attemptions - user.attempt === 1){
+            prize.currentPossible = prize.thirdPossible * prize.continuousFactor;
+            continue;
+        }
+
+        prize.total = 0;
+        user.attempt = 0;
         alert(`
         Thank you for your participation. 
         Your prize is: ${prize.total}$
@@ -60,11 +129,15 @@ for(let bufferConversionChecker, diceNumber, casinoAttemptsCounter = 1;
             Do you want to play again?
         `);
         if(willPlay){
-            // wants to play then new cycle
-            continue; 
-        }else{
-            // won't repeat cycle
-            willPlay = false;
+
+             range.end = 8;
+
+             prize.continuousFactor = 1;
+             prize.total = 0;
+             prize.firstPossible = 100;
+             prize.secondPossible = Math.floor(prize.firstPossible / 2);
+             prize.thirdPossible = Math.floor(prize.secondPossible /2);
+             prize.currentPossible = prize.firstPossible;
         }
     }
 }
