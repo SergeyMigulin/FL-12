1d587bb4d49770544dd67b690fea851a53ca930c
Sergey Migulin <sergiomigulini@gmail.com>
2020-01-19 00:13:18 +0200

    check

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -4,37 +4,45 @@ function Fighter(objPropFighter){
         let hp = objPropFighter.hp;
         let strength = objPropFighter.strength;
         let agility = objPropFighter.agility;
+        let wins = 0;
+        let losses = 0;
 
-    getName(){
+    this.getName = function (){
         return name;
     }
-    getDamage(){
+    this.getDamage = function (){
         return damage;
     }
-    getHealth(){
+    this.getHealth = function (){
         return hp;
     }
-    getStrength(){
+    this.getStrength = function (){
         return strength;
     }
-    getAgility(){
+    this.getAgility = function (){
         return agility;
     }
-    attack(fighter){
+    this.attack = function (fighter){
         let buff;
-        let probabilityOfAttack = (buff = fighter.agility + fighter.strength == 50) 
-            ? buff +1 
+        let probabilityOfAttack = (buff = fighter.agility + fighter.strength) === 50
+            ? buff + 1 
             : 100 - buff;
         let successfulRandomForAttack = Math.floor( Math.random() * 100) + 1;
     
         if(successfulRandomForAttack >= probabilityOfAttack){
-            fighter.hp = fighter.hp - getDamage();
-            console.log(`${getName} makes ${getDamage()} damage to ${fighter.name}`);
+            fighter.hp = fighter.hp - this.getDamage();
+            console.log(`${getName} makes ${this.getDamage()} damage to ${fighter.name}`);
         }else{
             console.log(`${fighter.name} attack missed`);
         }
     }
-    logCombatHistory(){
-
+    this.logCombatHistory = function (){
+        console.log(`Name: ${this.getName()}, Wins: ${wins}, Losses: ${losses}`);
     }
 }
+
+let f1 = new Fighter({name: 'axi', damage: 35, hp: 100, strength: 40, agility: 25});
+let f2 = new Fighter({name: 'mu', damage: 25, hp: 100, strength: 30, agility: 35});
+
+f1.logCombatHistory();
+console.log(f1.getName());
